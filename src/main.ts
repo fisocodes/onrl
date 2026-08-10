7690dc3c79431fa73417c6deeac2f37d19147b00
Oscar Figueroa <fisocodes@gmail.com>
2026-08-10 16:04:49 -0700

    feat: implemented logger module

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
+import { Logger } from "nestjs-pino";
 import { AppModule } from "./app.module";
 import { ConfigurationService } from "./configuration/configuration.service";
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+  });
   const configurationService = app.get(ConfigurationService);
+  app.useLogger(app.get(Logger));
   await app.listen(configurationService.appPort);
 }
 bootstrap();
