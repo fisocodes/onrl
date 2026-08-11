d5814ec558db7407a5efaf78586cc97e5da4670b
Oscar Figueroa <fisocodes@gmail.com>
2026-08-10 17:47:31 -0700

    feat: implemented Open API reference with Swagger and Scalar

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { apiReference } from "@scalar/nestjs-api-reference";
 import { Logger } from "nestjs-pino";
+import { version } from "../package.json";
 import { AppModule } from "./app.module";
 import { ConfigurationService } from "./configuration/configuration.service";
 
@@ -19,6 +22,16 @@ async function bootstrap() {
       whitelist: true,
     })
   );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("ONRL API")
+    .setDescription("ONRL API documentation")
+    .setVersion(version)
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  app.use("/reference", apiReference({ content: swaggerDocument }));
 
   const configurationService = app.get(ConfigurationService);
   await app.listen(configurationService.appPort);
